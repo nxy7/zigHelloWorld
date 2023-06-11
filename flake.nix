132e2dda1830c0e1b8299d932ffcc341d12c962f
nxyt <lolnoxy@gmail.com>
2023-06-11 23:34:38 +0200

    update

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: 
+  let
+    pkgs = import nixpkgs{
+      system = "x86_64-linux";      
+    };
+  in{
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = with pkgs; [
+        zig
+        zls
+      ];
+    };
+
+  };
+}
